46908e9b640f3cacb30cac48b809d8f73306a9b7
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2023-12-29 16:06:51 +0530

    Setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style>
+#app {
+  @apply select-none h-screen bg-gray-500;
+}
+</style>
